d2a06c472c6c03e319757ce85fc1b5f245771826
Ankit <asaha981119@gmail.com>
2018-07-04 15:25:06 +0530

    Questions.php v1

---
+++ questions.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+  <title>Quiz It!!</title>
+</head>
+<body>
+  <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="mx-auto order-0">
+      <a class="navbar-brand" href="#"><h1>Questions</h1></a>
+              </div>
+    </nav>  
+    <form>
+      <div class="row justify-content-sm-center">
+        <div class="col-sm-8">
+          <div class="form-group">
+            <label for="exampleFormControlTextarea1">Your Question ?</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
+        </div>
+      </div>
+      <div class="row justify-content-sm-center">
+        <div class="col-sm-4">
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Option 1?</label>
+            <input type="text" name="op1" placeholder="Enter one Option" required="required" class="form-control" id="exampleFormControlInput1">
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Option 2?</label>
+            <input type="text" name="op2" placeholder="Enter one Option" required="required" class="form-control" id="exampleFormControlInput1">
+          </div>
+        </div>
+        <div class="w-100"></div>
+        <div class="col-sm-4">
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Option 3?</label>
+            <input type="text" name="op3" placeholder="Enter one Option" required="required" class="form-control" id="exampleFormControlInput1">
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Option 4?</label>
+            <input type="text" name="op4" placeholder="Enter one Option" required="required" class="form-control" id="exampleFormControlInput1">
+          </div>
+        </div>
+        <div class="w-100"></div>
+        <div class="col-sm-4">
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Correct Answer?</label>
+            <input type="text" name="cop" placeholder="Enter one Option" required="required" class="form-control" id="exampleFormControlInput1">
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+</body>
+</html>
